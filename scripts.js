5b699f6dcde8879d7e3940af89ff2d92797340be
Genesses fernandes <102777715+Genesses12@users.noreply.github.com>
2023-06-11 13:11:55 -0400

    Conversor de unidades

---
+++ scripts.js
@@ -0,0 +1,64 @@
+//selecionar os elementos
+const inputElement = document.querySelector("#input");
+const fromElement = document.querySelector("#from");
+const toElement = document.querySelector("#to");
+const outputElement = document.querySelector("#output");
+const convertbutton = document.querySelector("#convert-btn");
+const messageElement = document.querySelector("#message");
+    
+    //Função para converter as unidades
+    function convert (){
+        const fromValue = fromElement.value;
+        const toValue = toElement.value;
+        if(fromValue === toValue){
+            outputElement.value = inputElement.value;
+            messageElement.textContent = "";
+            return; 
+        }  
+    
+    //converter a entrada para metros
+    let meters;
+    switch(fromValue){
+        case "m":
+            meters = inputElement.value;
+            break;
+            case "km":
+            meters = inputElement.value * 1000;
+            break;
+            case "cm":
+            meters = inputElement.value / 100;
+            break;
+            case "mm":
+            meters - inputElement.value / 1000;
+            break;
+    }
+// convertermetros para unidade de saida
+    let result;
+    switch(toValue){
+        case "m":
+            result = meters;
+            break;
+            case "km":
+                result = meters / 1000;
+                break;
+            case "cm":
+                result = meters * 100;
+                break;
+            case "mm":
+                result = meters * 1000;
+                break;    
+    }
+        //exibir resultado no input
+        outputElement.value = result;
+        //exibir resultado da mensagem
+        const fromLabel = fromElement.options[fromElement.selectedIndex].text;
+        const toLabel = toElement.options[toElement.selectedIndex].text;
+
+        const message = `${inputElement.value} ${fromLabel} equivalem a ${toLabel}`;
+        messageElement.textContent = message;
+        return;
+}
+    convertbutton.addEventListener("click", convert);
+    
+  
+    
